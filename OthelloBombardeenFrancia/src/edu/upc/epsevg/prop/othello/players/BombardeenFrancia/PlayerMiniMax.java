bf27525a77e92803cdaa9a570c9eb2f80b3ba0a3
Gx3 Studios <gabriel.guardiola@estudiantat.upc.edu>
2022-11-22 12:09:43 +0100

    Library-linked commit
    blablablabla

---
+++ OthelloBombardeenFrancia/src/edu/upc/epsevg/prop/othello/players/BombardeenFrancia/PlayerMiniMax.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package edu.upc.epsevg.prop.othello.players.BombardeenFrancia;
+
+import edu.upc.epsevg.prop.othello.GameStatus;
+import edu.upc.epsevg.prop.othello.IAuto;
+import edu.upc.epsevg.prop.othello.IPlayer;
+import edu.upc.epsevg.prop.othello.Move;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class PlayerMiniMax implements IPlayer, IAuto{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[])  {
+        // TODO code application logic here
+    }
+
+    @Override
+    public Move move(GameStatus gs) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void timeout() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
